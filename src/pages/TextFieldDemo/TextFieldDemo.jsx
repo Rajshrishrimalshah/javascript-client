7fad6ae4d2b044f3922468d00ed0eb6f31dd0646
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-05-29 19:44:00 +0530

    feat: added SliderComponet Features

--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
 import { InputBox } from "../../components/TextField/TextField";
+import  Slider  from "../../components/Slider/SilderComponent";
+import { banner} from "../../configs/constant";
+
+
 
 const TextFieldDemo = () => (
+<>
+  <div style = {{
+    display:"flex",
+    justifyContent:"center"
+  }}>
+  <Slider
+  altText = "Default Banner"
+  banner={banner}
+  defaultBanner = '../../../public/images/default.png'
+  duration = {2000}
+  height = "200px"
+  random = {false}
+   />
+
+
+  </div>
+
   <div
-    style={{
+    style= {{
       border: "1px solid ",
       margin: "100px 100px",
       padding: "5px 5px"
@@ -20,8 +41,10 @@ const TextFieldDemo = () => (
     <InputBox value="101" error="101" />
     <p style={{ color: "red" }}>could not be greater than</p>
 
-
   </div>
+
+
+</>
 );
 
 export default TextFieldDemo;
