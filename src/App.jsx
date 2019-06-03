60ab0052bfe56a4a9925d1c750d633e4b024ac90
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-06-03 17:35:28 +0530

    feat: Math component and template added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import theme from "./theme";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { MuiThemeProvider }from '@material-ui/core/styles';
+import ChildrenDemo from "./pages/childrenDemo/childrenDemo";
 
-import InputDemo from "./pages/InputFieldDemo/InputDemo";
+// import InputDemo from "./pages/InputFieldDemo/InputDemo";
 
 const App = () =>
   <MuiThemeProvider  theme={theme}>
-      <InputDemo />
+      <ChildrenDemo />
+      {/* <InputDemo /> */}
   </MuiThemeProvider>
 
 export default App;
--- src/components/Math/Math.jsx
+++ src/components/Math/Math.jsx
@@ -1,34 +1,27 @@
 import React, { Component } from 'react'
 
 class Math extends Component {
-
   calculator = (num1,operator,num2) => {
     switch(operator) {
     case '+' : return num1 + num2;
-                break;
-
     case '-' : return num1 - num2;
-                break;
-
     case '*' : return num1 * num2;
-                break;
-
-    case '/' : if(num === 2)
-                  return "Infinity"
+    case '/' : if(num2 === 0)
+                return "Infinity"
 
                 return num1 / num2;
-                break;
 
-    default : return "Incorrect Choice"
-
+    default : return "Incorrect Operation"
     }
-
   }
 
   render() {
-
+    const { first, second, operator, children } = this.props;
+    const result = this.calculator(first,operator,second);
     return(
-
+      React.cloneElement(children(result))
     );
   }
 }
+
+export default Math;
