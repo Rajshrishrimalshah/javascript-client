2d858941dc34883cdf5150c8dfe3465ea61f20d4
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-06-05 20:21:14 +0530

    fix: added NOMatch

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,33 @@ import ChildrenDemo from "./pages/childrenDemo"
 import React from 'react';
 import theme from "./theme";
 import { MuiThemeProvider }from '@material-ui/core/styles';
+import NoMatch from "./pages/NoMatch"
 // import ChildrenDemo from "./pages/childrenDemo/childrenDemo";
 import Trainee from "./pages/Trainee/Trainee";
 import Login from "./pages/Login";
+import { BrowserRouter, Switch, Route, } from 'react-router-dom'
 // import InputDemo from "./pages/InputFieldDemo/InputDemo";
 
 const App = () =>
     <MuiThemeProvider theme={theme}>
-      <ButtonAppBar />
-      <br/>
-      <Trainee />
+        <BrowserRouter>
+        <Switch>
+              <Route exact path='/' component={ButtonAppBar} />
+              <Route path='/login' component={Login} />
+              <Route path='/about' component={ButtonAppBar} />
+              <Route component={NoMatch} />
+
+        </Switch>
+        </BrowserRouter>
+
+
     </MuiThemeProvider>
   //<ChildrenDemo />
   //<Trainee />
   //<InputDemo>
   //<Login />
+  // <br/>
+  // <Trainee />
 
 
 export default App;
---
+++ src/pages/NoMatch/NoMatch.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const style = {
+  display:"flex",
+  justifyContent:"center",
+  alignItems:"center"
+}
+export default () => {
+  return (
+    <div style={{style}}>
+      <h1> Page Not Found</h1>
+      <h3> You seems to be on wrong page ! </h3>
+    </div>
+  );
+};
