7e3ab1243c270b79d059c236bd7a6e7452d688d5
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-07-02 13:50:56 +0530

    fix: styling

--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -2,26 +2,26 @@ import React from "react";
 
 import { InputBox } from "../../components/TextField/TextField";
 
-const TextFieldDemo = () => (
-  <div
-    style={{
-      border: "1px solid ",
-      margin: "100px 100px",
-      padding: "5px 5px"
-    }}
-  >
-    <h4>This is Disabled Input</h4>
-    <InputBox value="Disable Input" disabled />
+const TextFieldDemo = () => {
+  return (
+    <div
+      style={{
+        border: "1px solid ",
+        margin: "100px 100px",
+        padding: "5px 5px"
+      }}
+    >
+      <h4>This is Disabled Input</h4>
+      <InputBox value="Disable Input" disabled />
 
-    <h4>A valid box</h4>
-    <InputBox value="Accessible" />
+      <h4>A valid box</h4>
+      <InputBox value="Accessible" />
 
-    <h4>input with an error</h4>
-    <InputBox value="101" error="101" />
-    <p style={{ color: "red" }}>could not be greater than</p>
-
-
-  </div>
-);
+      <h4>input with an error</h4>
+      <InputBox value="101" error="101" />
+      <p style={{ color: "red" }}>could not be greater than</p>
+    </div>
+  );
+};
 
 export default TextFieldDemo;
